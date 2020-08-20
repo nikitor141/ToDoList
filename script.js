bed99bfc6d879b0264ef4d9d75c0b2b40553b68a
nikitor141 <68957821+nikitor141@users.noreply.github.com>
2020-08-20 14:49:33 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,70 @@
 $(function() {
-  $('.cross')
-    .click(function(){
-      $(this).toggle().parent().parent().parent().css('background-color', 'red');
+  $('.cross').click(function() {
+    $(this)
+      .parents('.left-white-box')
+      .remove();
+
 
     });
+
+
+$('.cross').click(function(){
+
+  if($('#box-parent').children().length < 1) {
+
+
+
+      $('#left-column').append('<span>Список пуст...</span>');
+
+      $('#left-h2')
+      .css('margin-bottom', '74px');
+      $('span')
+      .css({
+        'font-size': '16px',
+        'color': '#8993ad'
+      });
+
+
+  }
+})
+
+
+
+
+
+
+
+
+
+    /*
+    $('.cross').each(function(){
+
+      if($(this).children(none) ){
+        $(this).css('');
+
+
+      }
+
+
+    }));
+
+
+
+$('.cross').first().click(function(){
+
+  $('#left-column').append('<span>Список пуст...</span>');
+
+  $('#left-h2')
+  .css('margin-bottom', '74px');
+  $('span')
+  .css({
+    'font-size': '16px',
+    'color': '#8993ad'
+  });
+
+});*/
+
+
+
+
 });
