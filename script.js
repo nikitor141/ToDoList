7785ebd4ba6dc00cf6024cb7ad35d30b4aba0c75
nikitor141 <68957821+nikitor141@users.noreply.github.com>
2020-08-29 15:01:49 +0500

    Update script.js

--- script.js
+++ script.js
@@ -26,45 +26,49 @@ $('.cross').click(function(){
 
 
   }
-})
+});
+
+$('#right-button').click(function(){
+//get_right_name();
+//get_right_disc();
+//$('#right-name').val(function get_right_disc())
+//let input = document.querySelector('input');
+$('#box-parent').append(($(get_right_name()))) ;
+});
+
+
+
+function get_right_name(){
+
+  alert($('#right-name').val());
+};
+//function get_right_disc() {
+
+  //$(this).val(($('#right-disc')))
+//}
 
 
 
 
 
+/*$('#right-button').click(function(){
+  $('#box-parent').html('<div class="left-white-box">
 
+    ''<div class="left-top-box">
+      <div class="left-white-content">
+        <h3 class="right-h3  left-h3">Купить лошадь</h3>
+        <button class="cross"></button>
+      </div>
+    </div>
 
+    <div class="left-bottom-box">
+      <div class="left-white-content">
+        <p class="left-disc">По цене 300 т.р. за штуку </p>
+      </div>
+    </div>
 
-
-    /*
-    $('.cross').each(function(){
-
-      if($(this).children(none) ){
-        $(this).css('');
-
-
-      }
-
-
-    }));
-
-
-
-$('.cross').first().click(function(){
-
-  $('#left-column').append('<span>Список пуст...</span>');
-
-  $('#left-h2')
-  .css('margin-bottom', '74px');
-  $('span')
-  .css({
-    'font-size': '16px',
-    'color': '#8993ad'
-  });
-
-});*/
-
-
+  </div>')
+})*/
 
 
 });
