d9969a0d74fd209606c4ec62fae43ba893136074
nikitor141 <68957821+nikitor141@users.noreply.github.com>
2020-09-03 15:16:19 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,34 @@
 $(function() {
+
+
+
+
+
+
+  $('#right-button').click(function(){
+if($('#box-parent').children().length < 3){
+    $('#box-parent').append('<div class="left-white-box"><div class="left-top-box"><div class="left-white-content"><h3 class="right-h3  left-h3"></h3><button class="cross"></button></div></div><div class="left-bottom-box"><div class="left-white-content"><p class="left-disc"></p></div></div></div>')
+
+
+
+    var get_right_name = $('#right-name').val();//Получаем данные из input
+    $('.left-h3').last().html(get_right_name);//Вставляем значение в тег
+
+    var get_right_disc = $('#right-disc').val();
+    $('.left-disc').last().html(get_right_disc);
+}
+
+  });
+
   $('.cross').click(function() {
     $(this)
-      .parents('.left-white-box')
-      .remove();
+    .parents('.left-white-box')
+    .remove();
+  });
 
+  $('.cross').click(function() {
 
-    });
-
-
-$('.cross').click(function(){
-
-  if($('#box-parent').children().length < 1) {
-
-
+    if ($('#box-parent').children().length < 1) {
 
       $('#left-column').append('<span>Список пуст...</span>');
 
@@ -23,52 +39,11 @@ $('.cross').click(function(){
         'font-size': '16px',
         'color': '#8993ad'
       });
-
-
-  }
-});
-
-$('#right-button').click(function(){
-//get_right_name();
-//get_right_disc();
-//$('#right-name').val(function get_right_disc())
-//let input = document.querySelector('input');
-$('#box-parent').append(($(get_right_name()))) ;
-});
+    }
+  });
 
 
 
-function get_right_name(){
-
-  alert($('#right-name').val());
-};
-//function get_right_disc() {
-
-  //$(this).val(($('#right-disc')))
-//}
-
-
-
-
-
-/*$('#right-button').click(function(){
-  $('#box-parent').html('<div class="left-white-box">
-
-    ''<div class="left-top-box">
-      <div class="left-white-content">
-        <h3 class="right-h3  left-h3">Купить лошадь</h3>
-        <button class="cross"></button>
-      </div>
-    </div>
-
-    <div class="left-bottom-box">
-      <div class="left-white-content">
-        <p class="left-disc">По цене 300 т.р. за штуку </p>
-      </div>
-    </div>
-
-  </div>')
-})*/
 
 
 });
