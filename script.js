bbcfe50a62f8c925102ce7c3c736a41dd9d382ad
nikitor141 <68957821+nikitor141@users.noreply.github.com>
2020-09-10 18:12:56 +0300

    Update script.js

--- script.js
+++ script.js
@@ -5,61 +5,62 @@ $(function() {
 
 
 
-function span_css(){   $('#left-h2')
-  .css('margin-bottom', '74px');
-  $('span')
-  .css({
-    'font-size': '16px',
-    'color': '#8993ad'
-  });
-}
+  function span_css() {
+    $('#left-h2')
+      .css('margin-bottom', '74px');
+    $('span')
+      .css({
+        'font-size': '16px',
+        'color': '#8993ad'
+      });
+  }
 
 
 
-($('#right-name , #right-disc').on('keyup', function(){
-if($('#right-name , #right-disc').val('')){
-  $('#right-button').attr('disabled', 'disabled')
-}else{
-  $('#right-button').removeattr('disabled' , 'disabled')
-}
+  // ($('#right-button').on('click', function() {
+  //   if ($('#right-name , #right-disc')(.val().length < 1)) {
+  //     $('#right-button').attr('disabled', 'disabled')
+  //   } else {
+  //     $('#right-button').removeAttr('disabled')
+  //   }
+  //
+  // }))
 
-}))
 
-
-  $('#right-button').click(function(){
-    
-    
-    
-if($('#box-parent').children().length < 3){
-    $('#box-parent').append('<div class="left-white-box"><div class="left-top-box"><div class="left-white-content"><h3 class="right-h3  left-h3"></h3><button class="cross"></button></div></div><div class="left-bottom-box"><div class="left-white-content"><p class="left-disc"></p></div></div></div>');
+  $('#right-button').click(function() {
 
 
 
+    if ($('#box-parent').children().length < 3) {
+      $('#box-parent').append('<div class="left-white-box"><div class="left-top-box"><div class="left-white-content"><h3 class="right-h3  left-h3"></h3><button class="cross"></button></div></div><div class="left-bottom-box"><div class="left-white-content"><p class="left-disc"></p></div></div></div>');
 
-    var get_right_name = $('#right-name').val();//Получаем данные из input
-    $('.left-h3').last().html(get_right_name);//Вставляем значение в тег
 
-    var get_right_disc = $('#right-disc').val();
-    $('.left-disc').last().html(get_right_disc);
-}
+
+
+      var get_right_name = $('#right-name').val(); //Получаем данные из input
+      $('.left-h3').last().html(get_right_name); //Вставляем значение в тег
+
+      var get_right_disc = $('#right-disc').val();
+      $('.left-disc').last().html(get_right_disc);
+    }
 
     $('#right-name , #right-disc').val('');
 
 
-$('#left-h2').css('margin-bottom', '29px')
-$('span').remove()
-if($('#right-name', '#right-disc').val('')){
+    $('#left-h2').css('margin-bottom', '29px')
+    $('span').remove()
+    if ($('#right-name', '#right-disc').val('')) {
 
-}
+    }
   });
 
 
 
 
-  $('body').on("click",'.cross' ,function() {
+  $('body').on("click", '.cross', function() {
     $(this)
-    .parents('.left-white-box')
-    .remove();
+      .parents('.left-white-box')
+      .remove();
     if ($('#box-parent').children().length < 1) {
       $('#left-column').append('<span>Список пуст...</span>');
       span_css();
@@ -67,10 +68,10 @@ if($('#right-name', '#right-disc').val('')){
   });
 
 
-if ($('#box-parent').children().length < 1) {
-  $('#left-column').append('<span>Список пуст...</span>');
-  span_css();
-}
+  if ($('#box-parent').children().length < 1) {
+    $('#left-column').append('<span>Список пуст...</span>');
+    span_css();
+  }
 
 
 
